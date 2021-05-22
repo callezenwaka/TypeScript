c8406b87690f641d4fe4742697e8f3630ab5054d
callezenwaka <callisezenwaka@gmail.com>
2021-05-22 23:11:16 +0100

    Initial commit

---
+++ NamescapeExample/build/module08_exercise.js
@@ -0,0 +1,26 @@
+"use strict";
+var AllGreetings;
+(function (AllGreetings) {
+    let Greetings;
+    (function (Greetings) {
+        function returnGreeting(greeting) {
+            console.log(`The message from namespace Greetings is ${greeting}.`);
+        }
+        Greetings.returnGreeting = returnGreeting;
+    })(Greetings = AllGreetings.Greetings || (AllGreetings.Greetings = {}));
+    let GreetingsWithLength;
+    (function (GreetingsWithLength) {
+        function returnGreeting(greeting) {
+            let greetingLength = getLength(greeting);
+            console.log(`The message from namespace GreetingsWithLength is ${greeting}. It is ${greetingLength} characters long.`);
+        }
+        GreetingsWithLength.returnGreeting = returnGreeting;
+        function getLength(message) {
+            return message.length;
+        }
+    })(GreetingsWithLength = AllGreetings.GreetingsWithLength || (AllGreetings.GreetingsWithLength = {}));
+})(AllGreetings || (AllGreetings = {}));
+AllGreetings.Greetings.returnGreeting('Bonjour'); // OK
+AllGreetings.GreetingsWithLength.returnGreeting('Hola'); // OK
+var greet = AllGreetings.Greetings;
+greet.returnGreeting('Bonjour');
